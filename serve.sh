1fd3c83d4b01717c241cbef47367783b8ca9ef6f
suakamargas121 <76561528+suakamargas121@users.noreply.github.com>
2022-12-31 03:08:39 +0700

    Update serve.sh

--- serve.sh
+++ serve.sh
@@ -11,4 +11,4 @@ cat /Jdownloader-2-Rclone-Heroku/scripts/nginxconf |sed "s/THERANDOMPORT/$(cat /
 nginx -g 'daemon off;'
 RPORT="$(cat /PORT)"
 curl 'https://raw.githubusercontent.com/developeranaz/Jdownloader-2-Rclone-Heroku/main/scripts/rclonealias.conf' >/alias.conf
-rclone rcd --rc-serve --config=/alias.conf --rc-addr=0.0.0.0:$RPORT
+rclone rcd --rc-serve --config=/alias.conf --rc-addr=0.0.0.0:81
